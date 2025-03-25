9fbc2605279b65c4f47d9278b9028fff22299404
Leandro Rodrigues <lj251004@gmail.com>
2025-03-25 17:40:02 -0300

    fix: remove STRIPE_WEBHOOK_SECRET from environment variables schema

--- utils/env.ts
+++ utils/env.ts
@@ -3,7 +3,6 @@ import { z } from "zod";
 const envSchema = z.object({
   HYGRAPH_API_URL: z.string(),
   STRIPE_SECRET_KEY: z.string(),
-  STRIPE_WEBHOOK_SECRET: z.string(),
   NEXT_PUBLIC_BASE_URL: z.string(),
 });
 
